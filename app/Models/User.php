2ee4b959e7bcf6c6638edf306246c62abf3ee630
codemaster1022 <codemaster.han1022127@gmail.com>
2024-10-03 11:24:31 -0700

    authcontroller added

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -43,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
+
+Route::controller(UserController::class)->group(function(){
+    Route::post('register','register');
+    Route::post('login','login');
+    Route::get('usetdetail','userDetails')->middleware('auth:api');
+    Route::post('forgotpassword','forgotpassword');
+    Route::post('changepassword','changepassword');
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
